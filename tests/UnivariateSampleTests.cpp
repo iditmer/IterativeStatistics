512f94faba8d19026f068626f292f3111d022d6d
iditmer <90423241+iditmer@users.noreply.github.com>
2023-02-23 23:04:33 -1000

    Add test of single item count

--- tests/UnivariateSampleTests.cpp
+++ tests/UnivariateSampleTests.cpp
@@ -1,15 +1,23 @@
 
 #include <gtest/gtest.h>
-#include <vector>
 #include "UnivariateSample.hpp"
 
 class UnivariateSampleTest : public ::testing::Test {
 
 protected:
 
-    UnivariateSample no_items{ true, false };
+    UnivariateSample no_items{};
+    UnivariateSample single_item{};
+    
+    const double single_item_value = 3.1415926535;
+    
+    void SetUp() override {
+
+        single_item.Update(single_item_value);
+    }
 };
 
 TEST_F(UnivariateSampleTest, Count) {
     EXPECT_EQ(no_items.Count(), 0);
+    EXPECT_EQ(single_item.Count(), 1);
 }
