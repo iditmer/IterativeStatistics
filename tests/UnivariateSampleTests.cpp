f293809958446dc5e5379f680564ae5d6ad983fd
iditmer <90423241+iditmer@users.noreply.github.com>
2023-02-23 23:08:34 -1000

    Add test for 0 and 1 item means

--- tests/UnivariateSampleTests.cpp
+++ tests/UnivariateSampleTests.cpp
@@ -21,3 +21,8 @@ TEST_F(UnivariateSampleTest, Count) {
     EXPECT_EQ(no_items.Count(), 0);
     EXPECT_EQ(single_item.Count(), 1);
 }
+
+TEST_F(UnivariateSampleTest, Mean) {
+    EXPECT_THROW(no_items.Mean(), std::logic_error);
+    EXPECT_EQ(single_item.Mean(), single_item_value);
+}
