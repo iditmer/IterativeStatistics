1945301cc2a3ac4e31be711f1238778fa1b806ad
iditmer <90423241+iditmer@users.noreply.github.com>
2023-02-25 13:07:56 -1000

    Update test objects to include test for variance

--- tests/UnivariateSampleTests.cpp
+++ tests/UnivariateSampleTests.cpp
@@ -6,9 +6,11 @@ class UnivariateSampleTest : public ::testing::Test {
 
 protected:
 
-    UnivariateSample no_items{};
-    UnivariateSample single_item{};
-    UnivariateSample multiple_items{};
+    UnivariateSample no_items{ true };
+    UnivariateSample single_item{ false };
+    UnivariateSample single_item_with_variance{ true };
+    UnivariateSample multiple_items{ false };
+    UnivariateSample multiple_items_with_variance{ true };
     
     const double single_item_value = 3.1415926535;
 
@@ -21,13 +23,16 @@ protected:
     };
 
     const double multiple_item_mean = -1690.2929919999997;
+    const double multiple_item_variance = 21579307.70638082;
     
     void SetUp() override {
 
         single_item.Update(single_item_value);
+        single_item_with_variance.Update(single_item_value);
 
         for (auto& x : multiple_item_values) {
             multiple_items.Update(x);
+            multiple_items_with_variance.Update(x);
         }
     }
 };
@@ -35,11 +40,23 @@ protected:
 TEST_F(UnivariateSampleTest, Count) {
     EXPECT_EQ(no_items.Count(), 0);
     EXPECT_EQ(single_item.Count(), 1);
+    EXPECT_EQ(single_item_with_variance.Count(), 1);
     EXPECT_EQ(multiple_items.Count(), multiple_item_values.size());
+    EXPECT_EQ(multiple_items_with_variance.Count(), multiple_item_values.size());
 }
 
 TEST_F(UnivariateSampleTest, Mean) {
     EXPECT_THROW(no_items.Mean(), std::logic_error);
     EXPECT_EQ(single_item.Mean(), single_item_value);
+    EXPECT_EQ(single_item_with_variance.Mean(), single_item_value);
     EXPECT_NEAR(multiple_items.Mean(), multiple_item_mean, 1e-12);
+    EXPECT_NEAR(multiple_items_with_variance.Mean(), multiple_item_mean, 1e-12);
+}
+
+TEST_F(UnivariateSampleTest, Variance) {
+    EXPECT_THROW(no_items.Variance(), std::logic_error);
+    EXPECT_THROW(single_item.Variance(), std::logic_error);
+    EXPECT_EQ(single_item_with_variance.Variance(), 0.0);
+    EXPECT_THROW(multiple_items.Variance(), std::logic_error);
+    EXPECT_EQ(multiple_items_with_variance.Variance(), multiple_item_variance);
 }
