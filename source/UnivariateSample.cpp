44da6dfaf32d52aa6f5503466d9d651d32a3ff00
iditmer <90423241+iditmer@users.noreply.github.com>
2023-02-25 21:26:02 -1000

    Implement variance computation

--- source/UnivariateSample.cpp
+++ source/UnivariateSample.cpp
@@ -1,16 +1,27 @@
 
 #include "UnivariateSample.hpp"
-#include <stdexcept>
 
-UnivariateSample::UnivariateSample()
+UnivariateSample::UnivariateSample(bool track_variance)
 {
 	sum_x = 0;
+	sum_x_squared = 0;
 	count = 0;
+	track_var = track_variance;
+
+	if (track_variance) update = [this](double x) { this->update_with_var(x); };
+	else update = [this](double x) { this->update_base(x); };
 }
 
-void UnivariateSample::Update(double x)
+void UnivariateSample::update_base(double x) 
 {
 	sum_x += x;
+	count++;
+}
+
+void UnivariateSample::update_with_var(double x)
+{
+	sum_x += x;
+	sum_x_squared += x * x;
 	count++;
 }
 
@@ -18,4 +29,12 @@ double UnivariateSample::Mean() const
 {
 	if (count < 1) throw std::logic_error("No mean exists for sample of 0 elements.");
 	return sum_x / count;
+}
+
+double UnivariateSample::Variance() const
+{
+	if (!track_var) throw std::logic_error("Variance not tracked for this sample.");
+	else if (count == 0) throw std::logic_error("Variance cannot be computed from a collection of 0 elements.");
+	else if (count == 1) return 0.0;
+	else return (1.0 / (count - 1)) * (sum_x_squared - (1.0 / count) * sum_x * sum_x);
 }
--- source/UnivariateSample.hpp
+++ source/UnivariateSample.hpp
@@ -1,17 +1,30 @@
 #ifndef ITERATIVESTATISTICS_UNIVARIATESAMPLE_H
 #define ITERATIVESTATISTICS_UNIVARIATESAMPLE_H
 
+#include <functional>
+#include <stdexcept>
+
 class UnivariateSample
 {
 private:
+
 	unsigned int count;
 	double sum_x;
+	double sum_x_squared;
+	bool track_var;
+
+	std::function<void(double)> update;
+
+	void update_base(double x);
+	void update_with_var(double x);
 
 public:
-	UnivariateSample();
-	void Update(double x);
+
+	UnivariateSample(bool track_variance);
+	void Update(double x){ update(x); }
 
 	double Mean() const;
+	double Variance() const;
 	unsigned int Count() const { return count; }
 };
 
