5e7fcf30384de5c278dd131e03d72eac2bd88b52
iditmer <90423241+iditmer@users.noreply.github.com>
2023-02-27 23:28:35 -1000

    Add defintions for multivariate sample and its tests

---
+++ source/MultivariateSample.hpp
@@ -0,0 +1,20 @@
+#ifndef ITERATIVESTATISTICS_MULTIVARIATESAMPLE_H
+#define ITERATIVESTATISTICS_MULTIVARIATESAMPLE_H
+
+#include <vector>
+
+class MultivariateSample
+{
+private:
+
+	unsigned int count;
+
+public:
+
+	MultivariateSample() {}
+
+	unsigned int Count() const { return count; }
+
+};
+
+#endif // ITERATIVESTATISTICS_MULTIVARIATESAMPLE_H
