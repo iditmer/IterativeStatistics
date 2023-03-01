bd51365ac7cb6690748690d16d195cbe4e6b2726
iditmer <90423241+iditmer@users.noreply.github.com>
2023-02-28 22:39:09 -1000

    Add dimensionality as parameter to constructor

--- source/MultivariateSample.hpp
+++ source/MultivariateSample.hpp
@@ -8,12 +8,14 @@ class MultivariateSample
 private:
 
 	unsigned long long count;
+	int dimensionality;
 
 public:
 
-	MultivariateSample() { count = 0; }
+	MultivariateSample(int num_dimensions) { count = 0; dimensionality = num_dimensions; }
 	void Update(std::vector<double> vec) { count++; }
 	unsigned long long Count() const { return count; }
+	int Dimensionality() const { return dimensionality; }
 
 };
 
