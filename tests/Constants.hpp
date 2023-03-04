5224b8b7c426083037bfb26e627818c5eb45f088
iditmer <90423241+iditmer@users.noreply.github.com>
2023-03-03 17:41:15 -1000

    Extract univariate constants to separate header

---
+++ tests/Constants.hpp
@@ -0,0 +1,20 @@
+#ifndef ITERATIVE_STATISTICS_CONSTANTS_H
+#define ITERATIVE_STATISTICS_CONSTANTS_H
+
+const double univariate_single_item_value = 3.1415926535;
+
+const std::vector<double> univariate_multiple_item_values{
+    -7258.4216, -6672.0069, -612.7034, -3403.0569, -6951.4455,
+    6158.8905, -1234.1749, -6625.8533, 6407.0458, 4574.8612,
+    4724.2696, -2387.7143, -2184.6552, -2330.7389, 931.2824,
+    -3713.6563, 7305.6153, 1821.3522, -4241.8510, -5070.9812,
+    -6047.3554, 1658.9211, -2241.4804, -7381.3727, -7482.0950
+};
+
+const double univariate_multiple_item_mean = -1690.2929919999997;
+const double univariate_multiple_item_variance = 22478445.52748002;
+const double univariate_multiple_item_standard_deviation = 4741.143904953742;
+const double univariate_multiple_item_min = -7482.0950;
+const double univariate_multiple_item_max = 7305.6153;
+
+#endif
--- tests/UnivariateSampleTests.cpp
+++ tests/UnivariateSampleTests.cpp
@@ -1,5 +1,6 @@
 
 #include <gtest/gtest.h>
+#include "Constants.hpp"
 #include "UnivariateSample.hpp"
 
 class UnivariateSampleTest : public ::testing::Test {
@@ -18,30 +19,14 @@ protected:
     UnivariateSample multiple_items_with_extrema{ false, true };
     UnivariateSample multiple_items_with_both{ true,true };
     
-    const double single_item_value = 3.1415926535;
-
-    const std::vector<double> multiple_item_values{
-        -7258.4216, -6672.0069, -612.7034, -3403.0569, -6951.4455,
-        6158.8905, -1234.1749, -6625.8533, 6407.0458, 4574.8612,
-        4724.2696, -2387.7143, -2184.6552, -2330.7389, 931.2824,
-        -3713.6563, 7305.6153, 1821.3522, -4241.8510, -5070.9812,
-        -6047.3554, 1658.9211, -2241.4804, -7381.3727, -7482.0950 
-    };
-
-    const double multiple_item_mean = -1690.2929919999997;
-    const double multiple_item_variance = 22478445.52748002;
-    const double multiple_item_standard_deviation = 4741.143904953742;
-    const double multiple_item_min = -7482.0950;
-    const double multiple_item_max = 7305.6153;
-    
     void SetUp() override {
 
-        single_item.Update(single_item_value);
-        single_item_with_variance.Update(single_item_value);
-        single_item_with_extrema.Update(single_item_value);
-        single_item_with_both.Update(single_item_value);
+        single_item.Update(univariate_single_item_value);
+        single_item_with_variance.Update(univariate_single_item_value);
+        single_item_with_extrema.Update(univariate_single_item_value);
+        single_item_with_both.Update(univariate_single_item_value);
 
-        for (auto& x : multiple_item_values) {
+        for (auto& x : univariate_multiple_item_values) {
             multiple_items.Update(x);
             multiple_items_with_variance.Update(x);
             multiple_items_with_extrema.Update(x);
@@ -58,24 +43,24 @@ TEST_F(UnivariateSampleTest, Count) {
     ASSERT_EQ(single_item_with_extrema.Count(), 1);
     ASSERT_EQ(single_item_with_both.Count(), 1);
     
-    ASSERT_EQ(multiple_items.Count(), multiple_item_values.size());
-    ASSERT_EQ(multiple_items_with_variance.Count(), multiple_item_values.size());
-    ASSERT_EQ(multiple_items_with_extrema.Count(), multiple_item_values.size());
-    ASSERT_EQ(multiple_items_with_both.Count(), multiple_item_values.size());
+    ASSERT_EQ(multiple_items.Count(), univariate_multiple_item_values.size());
+    ASSERT_EQ(multiple_items_with_variance.Count(), univariate_multiple_item_values.size());
+    ASSERT_EQ(multiple_items_with_extrema.Count(), univariate_multiple_item_values.size());
+    ASSERT_EQ(multiple_items_with_both.Count(), univariate_multiple_item_values.size());
 }
 
 TEST_F(UnivariateSampleTest, Mean) {
     ASSERT_THROW(no_items.Mean(), std::logic_error);
 
-    ASSERT_EQ(single_item.Mean(), single_item_value);
-    ASSERT_EQ(single_item_with_variance.Mean(), single_item_value);
-    ASSERT_EQ(single_item_with_extrema.Mean(), single_item_value);
-    ASSERT_EQ(single_item_with_both.Mean(), single_item_value);
+    ASSERT_EQ(single_item.Mean(), univariate_single_item_value);
+    ASSERT_EQ(single_item_with_variance.Mean(), univariate_single_item_value);
+    ASSERT_EQ(single_item_with_extrema.Mean(), univariate_single_item_value);
+    ASSERT_EQ(single_item_with_both.Mean(), univariate_single_item_value);
 
-    ASSERT_NEAR(multiple_items.Mean(), multiple_item_mean, 1e-12);
-    ASSERT_NEAR(multiple_items_with_variance.Mean(), multiple_item_mean, 1e-12);
-    ASSERT_NEAR(multiple_items_with_extrema.Mean(), multiple_item_mean, 1e-12);
-    ASSERT_NEAR(multiple_items_with_both.Mean(), multiple_item_mean, 1e-12);
+    ASSERT_NEAR(multiple_items.Mean(), univariate_multiple_item_mean, 1e-12);
+    ASSERT_NEAR(multiple_items_with_variance.Mean(), univariate_multiple_item_mean, 1e-12);
+    ASSERT_NEAR(multiple_items_with_extrema.Mean(), univariate_multiple_item_mean, 1e-12);
+    ASSERT_NEAR(multiple_items_with_both.Mean(), univariate_multiple_item_mean, 1e-12);
 }
 
 TEST_F(UnivariateSampleTest, Variance) {
@@ -87,9 +72,9 @@ TEST_F(UnivariateSampleTest, Variance) {
     ASSERT_EQ(single_item_with_both.Variance(), 0.0);
 
     ASSERT_THROW(multiple_items.Variance(), std::logic_error);
-    ASSERT_NEAR(multiple_items_with_variance.Variance(), multiple_item_variance, 1e-7);
+    ASSERT_NEAR(multiple_items_with_variance.Variance(), univariate_multiple_item_variance, 1e-7);
     ASSERT_THROW(multiple_items_with_extrema.Variance(), std::logic_error);
-    ASSERT_NEAR(multiple_items_with_both.Variance(), multiple_item_variance, 1e-7);
+    ASSERT_NEAR(multiple_items_with_both.Variance(), univariate_multiple_item_variance, 1e-7);
 }
 
 TEST_F(UnivariateSampleTest, StandardDeviation) {
@@ -101,9 +86,9 @@ TEST_F(UnivariateSampleTest, StandardDeviation) {
     ASSERT_EQ(single_item_with_both.StandardDeviation(), 0.0);
 
     ASSERT_THROW(multiple_items.StandardDeviation(), std::logic_error);
-    ASSERT_NEAR(multiple_items_with_variance.StandardDeviation(), multiple_item_standard_deviation, 1e-11);
+    ASSERT_NEAR(multiple_items_with_variance.StandardDeviation(), univariate_multiple_item_standard_deviation, 1e-11);
     ASSERT_THROW(multiple_items_with_extrema.StandardDeviation(), std::logic_error);
-    ASSERT_NEAR(multiple_items_with_both.StandardDeviation(), multiple_item_standard_deviation, 1e-12);
+    ASSERT_NEAR(multiple_items_with_both.StandardDeviation(), univariate_multiple_item_standard_deviation, 1e-12);
 }
 
 TEST_F(UnivariateSampleTest, Minimum) {
@@ -111,13 +96,13 @@ TEST_F(UnivariateSampleTest, Minimum) {
 
     ASSERT_THROW(single_item.Min(), std::logic_error);
     ASSERT_THROW(single_item_with_variance.Min(), std::logic_error);
-    ASSERT_EQ(single_item_with_extrema.Min(), single_item_value);
-    ASSERT_EQ(single_item_with_both.Min(), single_item_value);
+    ASSERT_EQ(single_item_with_extrema.Min(), univariate_single_item_value);
+    ASSERT_EQ(single_item_with_both.Min(), univariate_single_item_value);
 
     ASSERT_THROW(multiple_items.Min(), std::logic_error);
     ASSERT_THROW(multiple_items_with_variance.Min(), std::logic_error);
-    ASSERT_EQ(multiple_items_with_extrema.Min(), multiple_item_min);
-    ASSERT_EQ(multiple_items_with_both.Min(), multiple_item_min);
+    ASSERT_EQ(multiple_items_with_extrema.Min(), univariate_multiple_item_min);
+    ASSERT_EQ(multiple_items_with_both.Min(), univariate_multiple_item_min);
 }
 
 TEST_F(UnivariateSampleTest, Maximum) {
@@ -125,11 +110,11 @@ TEST_F(UnivariateSampleTest, Maximum) {
 
     ASSERT_THROW(single_item.Max(), std::logic_error);
     ASSERT_THROW(single_item_with_variance.Max(), std::logic_error);
-    ASSERT_EQ(single_item_with_extrema.Max(), single_item_value);
-    ASSERT_EQ(single_item_with_both.Max(), single_item_value);
+    ASSERT_EQ(single_item_with_extrema.Max(), univariate_single_item_value);
+    ASSERT_EQ(single_item_with_both.Max(), univariate_single_item_value);
 
     ASSERT_THROW(multiple_items.Max(), std::logic_error);
     ASSERT_THROW(multiple_items_with_variance.Max(), std::logic_error);
-    ASSERT_EQ(multiple_items_with_extrema.Max(), multiple_item_max);
-    ASSERT_EQ(multiple_items_with_both.Max(), multiple_item_max);
+    ASSERT_EQ(multiple_items_with_extrema.Max(), univariate_multiple_item_max);
+    ASSERT_EQ(multiple_items_with_both.Max(), univariate_multiple_item_max);
 }
